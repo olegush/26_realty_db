3ea7497d6062d9511202886d1381cadfe5d43148
olegush <oleg107117@gmail.com>
2019-07-21 15:22:26 +0300

    changed import method

--- import.py
+++ import.py
@@ -12,15 +12,19 @@ except Exception as e:
     print(f'Backup error: {e}')
 
 try:
-    ads = Ad.query.filter_by().update({'active': False})
-    db.session.commit()
+    ads = db.session.query(Ad).filter(Ad.active == True)
+    ads_ids = set([ad.id for ad in ads])
     with open('db/ads.json') as file:
         new_ads = json.loads(file.read())
+    new_ads_ids = set((new_ad['id'] for new_ad in new_ads))
+    ads_ids_to_deactivate = ads_ids.difference(new_ads_ids)
+    ads = db.session.query(Ad).filter(Ad.id.in_(ads_ids_to_deactivate)).update({'active': False}, synchronize_session=False)
+    db.session.commit()
+    print(f'{len(ads_ids_to_deactivate)} ads were deactivated.')
     for new_ad in new_ads:
-        new_ad_obj = Ad(**new_ad)
-        merged = db.session.merge(new_ad_obj)
-    commited = db.session.commit()
-    print('{} new ads was exported with status "active". There are {} entries'
-        ' totally in the database.'.format(len(new_ads), len(Ad.query.all())))
+        db.session.merge(Ad(**new_ad))
+    db.session.commit()
+    print('{} new ads were upserted with status "active". \n'
+            'There are {} entries totally in the database.'.format(len(new_ads), len(Ad.query.all())))
 except Exception as e:
     print(f'Import error: {e}. Please restore you backup DB file and check your json file')
